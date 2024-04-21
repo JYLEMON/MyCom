a0ac62145e66973aa3a3a0f07fa921ce745d04d1
Jian Yang Tan <jytan-wm22@student.tarc.edu.my>
2024-04-21 14:42:45 +0800

    Error fix

--- app/src/main/java/com/example/mycom/ui/theme/Employee/EmployeeList.kt
+++ app/src/main/java/com/example/mycom/ui/theme/Employee/EmployeeList.kt
@@ -1,6 +1,5 @@
 package com.example.mycom.ui.theme.Employee
 
-import android.widget.ListView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
