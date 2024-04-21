e3d80d1e7fa02e78e069394d3580a4a91c533245
Jian Yang Tan <jytan-wm22@student.tarc.edu.my>
2024-04-21 14:33:29 +0800

    Testing Column

---
+++ app/src/main/java/com/example/mycom/ui/theme/Employee/EmployeeList.kt
@@ -0,0 +1,35 @@
+package com.example.mycom.ui.theme.Employee
+
+import android.widget.ListView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun EmployeeListScreen(
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier
+    ){
+
+       Row {
+           Box(modifier = modifier){
+               Text(text = "Test")
+           }
+       }
+    }
+}
+
+@Preview (
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun EmployeeListPreview() {
+    EmployeeListScreen()
+}
