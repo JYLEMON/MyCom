3c67db425ea58bf8ff0dbafa50913ae839c23dd6
Jian Yang Tan <jytan-wm22@student.tarc.edu.my>
2024-04-23 10:27:08 +0800

    Add Floating button

--- app/src/main/java/com/example/mycom/EmployeeScreen.kt
+++ app/src/main/java/com/example/mycom/EmployeeScreen.kt
@@ -1,7 +1,6 @@
 package com.example.mycom
 
 import androidx.compose.runtime.Composable
-import com.example.mycom.ui.theme.Employee.Employee
 
 @Composable
 fun EmployeeScreen() {
--- app/src/main/java/com/example/mycom/ui/theme/Employee/EmployeeList.kt
+++ app/src/main/java/com/example/mycom/ui/theme/Employee/EmployeeList.kt
@@ -1,22 +1,24 @@
-package com.example.mycom.ui.theme.Employee
-
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,14 +32,29 @@ fun EmployeeListScreen(
     employees: List<Employee>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier
-            .padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        items(employees) { employee ->
-            EmployeeListItem(employee = employee)
+        LazyColumn(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        ) {
+            items(employees) { employee ->
+                EmployeeListItem(employee = employee)
+            }
+        }
+        FloatingActionButton(
+            onClick = { /* Navigate to add employee screen */ },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+                .clip(CircleShape)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "Add Employee",
+            )
         }
     }
 }
@@ -70,7 +87,18 @@ fun EmployeeListItem(employee: Employee) {
 fun EmployeeListPreview() {
     val employees = listOf(
         Employee("John", "S001"),
-        Employee("Test", "S002")
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
+        Employee("Test", "S002"),
     )
     EmployeeListScreen(employees = employees)
 }
