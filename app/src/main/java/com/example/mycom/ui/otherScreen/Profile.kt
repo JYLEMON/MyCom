ddecb366ec99163f2d4691ddbd75f7b4920c0daa
Jian Yang Tan <jytan-wm22@student.tarc.edu.my>
2024-05-19 16:47:31 +0800

    Profile Done

--- app/src/main/java/com/example/mycom/ui/otherScreen/Profile.kt
+++ app/src/main/java/com/example/mycom/ui/otherScreen/Profile.kt
@@ -2,153 +2,176 @@ package com.example.myapplication.ui.theme.otherScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.Alignment
 import com.example.mycom.ui.theme.MyComTheme
 
-
 @Composable
-fun ProfileScreen(name:String,
-                  email: String,
-                  salary: String,
-                  work: String,
-                  notification: String,
-                  textbotton1: String,
-                  onClickButton1 : () -> Unit ={},
-                  onClickButton2 : () -> Unit ={},
-                  onClickButton3 : () -> Unit ={},
-                  onClickButton4 : () -> Unit ={},
-                  onEditClick : () -> Unit ={},
-                  modifier: Modifier = Modifier){
-    var name : String = "liangyouxian"
-    var email : String = "liangyouxian1@gmail.com"
-    var salary : String = "2000.50"
-    var work : String = "10"
-    var notivication : String = "Work/Staff Approval:"
-    var textbotton1 : String = "Approval Leave & Late"
-    Box (modifier = Modifier
-        .background(color = Color.Gray)
-        .fillMaxSize()){
-    Column (modifier = Modifier
-        .padding(top = 25.dp, end = 25.dp, start = 25.dp, bottom = 25.dp)
-        .fillMaxSize()
-        .background(Color.LightGray)) {
-        Column (modifier = Modifier
-            .padding(top = 25.dp, end = 25.dp, start = 25.dp, bottom = 25.dp)
-        ){
-        Card() {
-            Column {
-                Text(text = "$name", fontSize = 50.sp, modifier = Modifier.fillMaxWidth())
-                Text(text = "$email", modifier = Modifier.fillMaxWidth())
-            }
-
-            Text(text = "Your Salary:", fontSize = 20.sp)
-            Card(
-                colors = CardDefaults.cardColors(Color.White),
+fun ProfileScreen(
+    name: String,
+    empId: String,
+    email: String,
+    salary: String,
+    onClickButton2: () -> Unit = {},
+    onClickButton3: () -> Unit = {},
+    onEditClick: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
                 modifier = Modifier
-                    //.background(color = Color.White)
-                    .fillMaxWidth()
+                    .background(Color.LightGray)
             ) {
-                Row {
+                Column {
                     Text(
-                        text = "RM",
-                        fontSize = 25.sp,
-                        textAlign = TextAlign.Start
-                    )
-                    Text(
-                        text = "$salary",
-                        fontSize = 25.sp,
-                        textAlign = TextAlign.Start,
+                        text = name,
+                        fontSize = 40.sp,
                         modifier = Modifier
                             .fillMaxWidth()
+                            .padding(8.dp)
                     )
-                }
-            }
-            Text(text = "$notivication", fontSize = 20.sp)
-            Card(
-                colors = CardDefaults.cardColors(Color.White),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { onClickButton1() }
-            ) {
-                Row {
 
                     Text(
-                        text = "$work",
+                        text = empId,
                         fontSize = 25.sp,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
                     )
                 }
             }
-            Text(text = "$notivication", fontSize = 20.sp)
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
             Card(
-                colors = CardDefaults.cardColors(Color.White),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { onClickButton1() }
+                modifier = Modifier.padding(16.dp)
             ) {
-                Row {
+                Column(
+                    Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Email:", fontSize = 20.sp)
+                    Row {
+                        Text(
+                            text = email,
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Start
+                        )
+                    }
 
-                    Text(
-                        text = "$work",
-                        fontSize = 25.sp,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
+                    Divider(Modifier.padding(bottom = 8.dp))
+
+                    Text(text = "Salary:", fontSize = 20.sp)
+                    Row {
+                        Text(
+                            text = "RM ",
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Start
+                        )
+                        Text(
+                            text = salary,
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Start
+                        )
+                    }
+                    Divider(Modifier.padding(bottom = 8.dp))
+                }
+                Spacer(modifier = Modifier.padding(4.dp))
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .clickable { onClickButton2() }
+            ) {
+                Column(
+                    Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Application",
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Email,
+                            contentDescription = "Application"
+                        )
+                    }
                 }
             }
 
+            Spacer(modifier = Modifier.weight(1f))
+
+            OutlinedButton(
+                onClick = onClickButton3,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Logout",
+                    fontSize = 16.sp
+                    )
+            }
         }
-            Button(onClick = onClickButton2, Modifier.fillMaxWidth()) { Text(text = "$textbotton1") }
-            Button(onClick = onClickButton3, Modifier.fillMaxWidth()) { Text(text = "Logout") }
 
-    }
-
-    }
         FloatingActionButton(
             onClick = onEditClick,
             shape = CircleShape,
             modifier = Modifier
                 .align(alignment = Alignment.BottomEnd)
                 .padding(16.dp)
-
-
         ) {
             Icon(
                 imageVector = Icons.Filled.Edit, // Use the Edit icon
                 contentDescription = "Edit"
             )
         }
-}
+    }
 }
 
-@Preview
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
 @Composable
 fun profilePreview() {
     MyComTheme {
-        ProfileScreen(name = "shit", email ="liangyouxian1@gmail.com" , salary = "2000.50", work = "10", notification ="Work/Staff Approval:" , textbotton1 = "Approval Leave & Late")
+        ProfileScreen(
+            name = "Liang You Xian",
+            empId = "E001",
+            email = "liangyouxian1@gmail.com",
+            salary = "2000.50",
+        )
     }
 }
